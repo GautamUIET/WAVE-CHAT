8d8902593485ceb315398d7af7c1554f29ba479c
GautamUIET <gautamjain09687@gmail.com>
2024-10-16 19:32:03 +0530

    4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
